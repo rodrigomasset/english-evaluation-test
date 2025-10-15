1881f84d8971a3e761af159f9ac5d3f45052f7ac
rodrigomasset <rodrigocmasset@gmail.com>
2025-10-14 23:27:50 -0300

    Update db.ts

--- teste-ingles-mvp-full/app/lib/db.ts
+++ teste-ingles-mvp-full/app/lib/db.ts
@@ -1,5 +1,9 @@
 import { Pool } from 'pg'
+
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-export async function query<T=any>(text: string, params?: any[]): Promise<{ rows: T[] }> {
-  return pool.query(text, params)
+
+export async function query<T = any>(text: string, params?: any[]): Promise<{ rows: T[] }> {
+  const res = await pool.query(text, params)
+  // normaliza o formato para { rows: T[] }
+  return { rows: res.rows as T[] }
 }
