439b2d409a9623eb46e87b25ed9ae4b884e75900
rodrigomasset <rodrigocmasset@gmail.com>
2025-10-14 23:38:47 -0300

    Delete teste-ingles-mvp-full/lib/db.ts

--- teste-ingles-mvp-full/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import { Pool } from 'pg'
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-export async function query<T=any>(text: string, params?: any[]): Promise<{ rows: T[] }> {
-  return pool.query(text, params)
-}
